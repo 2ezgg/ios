583205a2d2d5f5b3fb3234b9676fdbf8af16e0b1
Jake Prather <JakeHP@Zoho.com>
2014-05-20 18:01:35 -0500

    added all links

---
+++ ios/LinkFactory.m
@@ -0,0 +1,77 @@
+//
+//  UrlLibrary.m
+//  ios
+//
+//  Created by Jake on 5/17/14.
+//  Copyright (c) 2014 2ez.gg. All rights reserved.
+//
+
+#import "LinkFactory.h"
+
+NSString *const LolKing                     = @"http://www.lolking.net";
+NSString *const LolKingChampSearch          = @"/champions/";
+NSString *const LolKingSearch               = @"/search?";
+
+NSString *const LolKingNow                  = @"http://www.lolking.net";
+
+NSString *const LolNexus                    = @"http://www.lolnexus.com";
+NSString *const LolNexusSearchEnd           = @"/search?";
+
+NSString *const LolSkill                    = @"http://www.lolskill.net";
+NSString *const LolSkillSearch              = @"/game-";
+
+NSString *const Summoning                   = @"http://www.summoning.net";
+NSString *const SummoningSearch             = @"/v1/lyralei";
+
+NSString *const Kassad                      = @"http://quickfind.kassad.in";
+NSString *const KassadSearch                = @"/profile/";
+
+NSString *const OPBase                      = @"http://";
+NSString *const OP                          = @"op.gg";
+NSString *const OPSearch                    = @"/summoner/userName=";
+
+NSString *const LolDb                       = @"http://loldb.gameguyz.com";
+NSString *const LolDbSearch                 = @"/analyze/search?search_text=";
+NSString *const LolDbServer                 = @"&c_server=1_10_6_2_3_4_5_7_8_9";
+NSString *const LolDbChampSearchStart       = @"/champions/";
+NSString *const LolDbChampSearchEnd         = @".html";
+
+NSString *const Elophant                    = @"http://www.elophant.com";
+NSString *const ElophantSearch              = @"/league-of-legends/search?query=";
+NSString *const ElophantChampSearchStart    = @"/league-of-legends/champion/";
+NSString *const ElophantChampSearchEnd      = @"/stats";
+
+NSString *const ChampSelect                 = @"http://www.championselect.net";
+NSString *const ChampSelectChampSearch      = @"/champ/";
+
+NSString *const TSM                         = @"http://www.solomid.net";
+NSString *const TSMChampSearch              = @"/guide?champ=";
+
+NSString *const ProBuilds                   = @"http://www.probuilds.net";
+NSString *const ProBuildsChampSearch        = @"/champions/";
+
+NSString *const MobaFire                    = @"http://www.mobafire.com";
+NSString *const MobaFireChampSearchStart    = @"/league-of-legends/";
+NSString *const MobaFireChampSearchEnd      = @"-guide";
+
+NSString *const LolBuilder                  = @"http://www.lolbuilder.net";
+NSString *const LolBuilderChampSearch       = @"/";
+
+NSString *const LolPro                      = @"http://www.lolpro.com";
+NSString *const LolProChampSearch           = @"/guides/";
+
+NSString *const LolWiki                     = @"http://leagueoflegends.wikia.com";
+NSString *const LolWikiChampSearch          = @"/wiki/";
+
+NSString *const LolGamepedia                = @"http://lol.gamepedia.com";
+NSString *const LolGamepediaChampSearch     = @"/";
+
+NSString *const LolInven                    = @"http://lol.inven.co.kr";
+NSString *const LolInvenChampSearch         = @"/dataninfo/champion/";
+NSString *const LolInvenChampDetail         = @"detail.php?code=";
+
+// linkFactory clone
+
+@implementation LinkFactory
+
+@end
